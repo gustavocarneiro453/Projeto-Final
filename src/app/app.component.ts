140b3ae8743900a545a39ff08e6f66c446610909
gustavocarneiro453 <gustavocidc@gmail.com>
2025-03-28 10:19:20 -0300

    em andamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router'; 
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component'; // ajuste o caminho se necessário
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
